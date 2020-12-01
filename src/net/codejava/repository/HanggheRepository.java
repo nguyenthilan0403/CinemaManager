8523485acbb729a2e62a61ae43e72565fe8db9d7
QuangHuyy <50898345+QuangHuyy@users.noreply.github.com>
2020-12-01 18:50:34 +0700

    Update repository
    HanggheRepository

---
+++ src/net/codejava/repository/HanggheRepository.java
@@ -0,0 +1,9 @@
+package net.codejava.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import net.codejava.model.Hangghe;
+
+public interface HanggheRepository extends CrudRepository<Hangghe, Long> {
+
+}
